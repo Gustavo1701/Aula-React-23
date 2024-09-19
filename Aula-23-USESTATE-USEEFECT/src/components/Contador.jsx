812bdda03b32401507377348d5157570bf696fd1
Gustavo1701 <gustavojr.lg.10@gmail.com>
2024-09-18 21:42:42 -0300

    Atualizando

--- Aula-23-USESTATE-USEEFECT/src/components/Contador.jsx
+++ Aula-23-USESTATE-USEEFECT/src/components/Contador.jsx
@@ -1,28 +1,32 @@
-import { useState } from "react"
-
+import { useState, useEffect } from 'react'
 function Contador() {
-
-    const [contador, setContador] = useState(0);
-
+    const[contador, setContador] = useState(0)
     function aumentar() {
-        setContador(contador + 1);
+        setContador(contador + 1)
+    }
+    function diminuir() {
+        setContador(contador - 1)
     }
 
-    function diminuir() {
-        setContador(contador - 1);
-    }
+    // Executa sempre quando o contador altera
+    useEffect(() => {
+        console.log(contador)
+        if(contador > 20) {
+            alert("Passando de 20")
+        }
+    }, [contador]);
+    
+    // Executa quando o componente cria na tela - APenas 1 vez
+    useEffect(() => {
+        // alert("Ola")
+    }, [])
 
     return (
         <>
-        <div className="contador">
-           <input type="text">{contador}</input>
+            Contador: {contador}
             <button onClick={aumentar}>Aumentar</button>
             <button onClick={diminuir}>Diminuir</button>
-
-        </div>
         </>
     )
-
 }
-
-export default Contador();
+export default Contador;
